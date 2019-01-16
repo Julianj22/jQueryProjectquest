55c1a9cc14de71d382ba610f8343f4f36c6d7818
Julianj22 <42346111+Julianj22@users.noreply.github.com>
2019-01-16 18:40:27 -0500

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ $("#one").click(function() {
     $("#one").hide();
 });
 
-$("#two").dblclick(function() {
+$("#awakening").dblclick(function() {
     $("#awakening").fadeOut(4000);
     $("#john").delay(5000).fadeIn(4000);
     $("#two").fadeOut(4000);
@@ -44,18 +44,40 @@ $(y).hover(function() {
 
     $("#john").fadeOut(4000);
     $("#gm").fadeOut(4000);
-    $("#ch").fadeOut(5000);
-    $("#d").fadeOut(6000);
+    $("#ch").fadeOut(4000);
+    $("#d").fadeOut(4000);
     $("#parents").delay(5000).fadeIn(4000);
     $("#gm2").delay(6000).fadeIn(4000);
     $("#gm3").delay(7000).fadeIn(4000);
-    $(z).delay(8000).fadeIn(4000);
+    $(".glyph2").delay(8000).fadeIn(4000);
 });
 
 $(z).click(function() {
-     $("#parents").fadeout(4000);
-    $("#gm2").fadeOut(4000);
-    $("#gm3").fadeOut(4000);
+     $("#mom").fadeOut();
+    $(z).hide();
+    $(b).hide();
     $("#las").show();
+    $("#tm").delay(2000).show();
+});
+
+$(b).click(function() {
+    $("#mom").fadeOut();
+     $(z).hide();
+    $(b).hide();
+    $("#md1").fadeIn();
+    $("#md2").fadeIn();
+    $("#md3").fadeIn();
+    $("#md4").fadeIn();
+    $("#quest").delay(10000).fadeOut(3000);
+    $("#reload").delay(18000).fadeIn();
+    $("#reload").css("margin-top","0");
+     $("#reload").css("margin-bottom","0");
+     $("#reload").css("margin-right","auto");
+     $("#reload").css("margin-left","auto");
+});
+
+
+$("#reload").click(function() {
+    location.reload();
 });
 
